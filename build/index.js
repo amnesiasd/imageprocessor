ccd644465b74618998b545abf8ab5d098e0e7ebf
Troy Hayman <THayman@innout.com>
2021-07-26 13:11:06 -0700

    Started server

--- build/index.js
+++ build/index.js
@@ -35,9 +35,21 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var indexUtils_1 = require("./utilities/indexUtils");
-var displayImages = function () { return __awaiter(void 0, void 0, void 0, function () {
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+var port = 3000;
+app.get('/api', function (req, res) {
+    res.send('server still working');
+});
+app.listen(port, function () {
+    console.log("server started at http://localhost:" + port);
+});
+var getImageArray = function () { return __awaiter(void 0, void 0, void 0, function () {
     var imgs;
     return __generator(this, function (_a) {
         switch (_a.label) {
@@ -49,4 +61,4 @@ var displayImages = function () { return __awaiter(void 0, void 0, void 0, funct
         }
     });
 }); };
-displayImages();
+getImageArray();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
 import {getImages} from "./utilities/indexUtils";
+import express from 'express';
 
-const displayImages = async () => {
-  const imgs = await getImages();
-  console.log(imgs);
-} 
-displayImages();
+const app = express();
+const port = 3000;
+
+const getImageArray = () => {
+  const imgs = getImages();
+  return imgs;
+}; 
+
+app.get('/api', (req, res) => {
+  
+  const getImageArray = async () => {
+    const imgs = await getImages();
+    res.send(imgs[1]);
+  };
+  getImageArray();
+  console.log(req.query);
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
+
+
+
+//getImageArray();
